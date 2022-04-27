49b99d9410773a2d44e483c27562d04aa130f4f8
Jessie <2302541+Kangaroux@users.noreply.github.com>
2022-04-27 19:37:51 -0400

    Temporarily remove type from TwitterMedia model

--- web/api/models.py
+++ web/api/models.py
@@ -15,11 +15,12 @@ class TwitterUser(BaseModel):
 
 
 class TwitterMedia(BaseModel):
-    TYPE_PHOTO = "photo"
-    TYPE_VIDEO = "video"
-    TYPE_CHOICES = [(TYPE_PHOTO, TYPE_PHOTO), (TYPE_VIDEO, TYPE_VIDEO)]
+    # TYPE_PHOTO = "photo"
+    # TYPE_VIDEO = "video"
+    # TYPE_CHOICES = [(TYPE_PHOTO, TYPE_PHOTO), (TYPE_VIDEO, TYPE_VIDEO)]
+
+    # type = models.CharField(max_length=20, choices=TYPE_CHOICES)
 
     user = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
     key = models.CharField(max_length=50)
     url = models.CharField(max_length=255)
