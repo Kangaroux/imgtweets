7aa19d72437966732ffc6708fccbcf2bebd42fe5
Jessie <2302541+Kangaroux@users.noreply.github.com>
2022-04-27 20:21:48 -0400

    Add a scrape command

---
+++ web/api/management/commands/scrape.py
@@ -0,0 +1,79 @@
+from unittest import result
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from django.db import transaction, IntegrityError
+from api.models import TwitterUser, TwitterMedia
+
+from lib.twitter import TwitterAPI, TwitterMediaType
+
+
+class Command(BaseCommand):
+    help = "Scrapes photos from a user's timeline"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-c",
+            "--count",
+            type=int,
+            default=50,
+            help="The max number of tweets to scrape.",
+        )
+        parser.add_argument(
+            "-t",
+            "--token",
+            default=settings.TWITTER_API_TOKEN,
+            help="The twitter API token. Defaults to the TWITTER_API_TOKEN env.",
+        )
+        parser.add_argument(
+            "username",
+            help="The twitter username to scrape.",
+        )
+
+    def handle(self, username: str, count: int, token: str, **options):
+        if not username:
+            raise CommandError("Username cannot be empty.")
+        elif count < 1 or count > 100:
+            raise CommandError("Count must be between [1, 100].")
+        elif not token:
+            raise CommandError(
+                "Twitter API token is missing. Please provide it with the --token option, "
+                "or set the TWITTER_API_TOKEN environment variable."
+            )
+
+        t = TwitterAPI(token)
+        u = t.get_user_by_username(username)
+        tweets, _ = t.get_user_media_tweets(u.id, limit=count)
+
+        print(f"Found {len(tweets)} tweets")
+
+        if not tweets:
+            return
+
+        try:
+            user = TwitterUser.objects.get(twitter_id=u.id)
+            print("Fetched existing user")
+        except TwitterUser.DoesNotExist:
+            user = TwitterUser.objects.create(twitter_id=u.id, username=u.username)
+            print("Created new user")
+
+        media_objects = []
+
+        for t in tweets:
+            for m in t.media:
+                if m.type != TwitterMediaType.Photo:
+                    continue
+
+                media_objects.append(TwitterMedia(user=user, key=m.key, url=m.url))
+
+        print(f"Found {len(media_objects)} media")
+
+        added = 0
+
+        for obj in media_objects:
+            try:
+                obj.save()
+                added += 1
+            except IntegrityError:
+                pass
+
+        print(f"Added {added} new media")
