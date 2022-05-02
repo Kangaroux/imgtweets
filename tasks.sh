19eee23e65a5431b55e5fd37d1989df6d20f0673
Jessie <2302541+Kangaroux@users.noreply.github.com>
2022-05-02 15:26:10 -0400

    Update static build script

--- tasks.sh
+++ tasks.sh
@@ -5,9 +5,29 @@ cd ${0%/*}
 
 case "$1" in
 "build")
+    echo '>> Building frontend'
     cd frontend
     yarn build
+
+    echo '>> Collecting server static files'
     cd ../server
-    ./manage.py collectstatic -c --no-input
+    rm -rf static/
+    ./manage.py collectstatic --no-input
+
+    # Copies the frontend and server assets to dist/
+    #
+    # dist/
+    #   index.html
+    #   static/
+    #     {css,js}/                 <- frontend assets
+    #     {admin,rest_framework}/   <- server assets
+    echo '>> Copying to dist/'
+    cd ..
+
+    rm -rf dist/ && mkdir -p dist/static/
+
+    cp -R server/static/* dist/static/
+    cp frontend/build/index.html dist/
+    cp -R frontend/build/static/* dist/static/
     ;;
 esac
