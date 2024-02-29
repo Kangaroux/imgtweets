9ad6e9a3b4147ac2f4fd65aeb9a220e2ba05fd32
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-02-29 00:22:35 -0500

    Removed react/backend from build steps

--- tasks.sh
+++ tasks.sh
@@ -5,14 +5,14 @@ cd ${0%/*}
 
 case "$1" in
 "build")
-    echo '>> Building frontend'
-    cd frontend
-    yarn build
+    # echo '>> Building frontend'
+    # cd frontend
+    # yarn build
 
-    echo '>> Collecting server static files'
-    cd ../server
-    rm -rf static/
-    ./manage.py collectstatic --no-input
+    # echo '>> Collecting server static files'
+    # cd ../server
+    # rm -rf static/
+    # ./manage.py collectstatic --no-input
 
     # Copies the frontend and server assets to dist/
     #
@@ -21,13 +21,12 @@ case "$1" in
     #   static/
     #     {css,js}/                 <- frontend assets
     #     {admin,rest_framework}/   <- server assets
-    echo '>> Copying to dist/'
-    cd ..
+    # echo '>> Copying to dist/'
+    # cd ..
 
-    rm -rf dist/ && mkdir -p dist/static/
+    rm -rf dist/ && mkdir -p dist/
 
-    cp -R server/static/* dist/static/
-    cp frontend/build/{index.html,robots.txt} dist/
-    cp -R frontend/build/static/* frontend/build/img/ dist/static/
+    # cp -R server/static/* dist/static/
+    cp -r frontend/public/* dist/
     ;;
 esac
